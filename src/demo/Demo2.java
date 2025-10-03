0eec0ec86ba70c64c033b568a247424341b35487
alex_ <alex_@localhost>
2025-10-03 12:46:57 +0200

    cambio master en luna

--- src/demo/Demo2.java
+++ src/demo/Demo2.java
@@ -6,7 +6,7 @@ package demo;
 public class Demo2 {
     public static void main(String[] args) {
 	System.out.println("Linea 1");
-	System.out.println("Linea 2");
+	System.out.println("Cambio master en liuna");
 	System.out.println("Linea 3");
     }
     /** cambio desde mars **/
