2cbb5cb4556b653f1847abf8badd6934dac2708e
alex_ <alex_@localhost>
2025-10-03 12:33:14 +0200

    cambio2 primer cambio

--- src/demo/Demo2.java
+++ src/demo/Demo2.java
@@ -1,10 +1,11 @@
 package demo;
 
+/** cambio desde luna **/
 public class Demo2 {
     public static void main(String[] args) {
 	System.out.println("Linea 1");
 	System.out.println("Linea 2");
 	System.out.println("Linea 3");
     }
-
+    /** cambio desde mars **/
 }
