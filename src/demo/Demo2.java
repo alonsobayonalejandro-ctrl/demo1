fe967999ee47268f6f728472545d6a51860e6edb
alex_ <alex_@localhost>
2025-10-03 12:34:34 +0200

    cambio desde master

--- src/demo/Demo2.java
+++ src/demo/Demo2.java
@@ -1,10 +1,12 @@
 package demo;
 
+/**cambio desde luna**/
+/** cambio2 desde luna en master **/
 public class Demo2 {
     public static void main(String[] args) {
 	System.out.println("Linea 1");
 	System.out.println("Linea 2");
 	System.out.println("Linea 3");
     }
-
+    /** cambio desde master **/
 }
